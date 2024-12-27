3751a2f0ceb5e3cd17d04a7971ab550ff35d4e6e
jonah <jonah@jsinco.dev>
2024-12-26 19:22:07 -0500

    push

--- src/main/java/dev/jsinco/luma/manager/guis/AbstractGui.java
+++ src/main/java/dev/jsinco/luma/manager/guis/AbstractGui.java
@@ -5,7 +5,7 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.InventoryHolder;
 
-interface AbstractGui extends InventoryHolder {
+public interface AbstractGui extends InventoryHolder {
 
     void onInventoryClick(InventoryClickEvent event);
     void onInventoryClose(InventoryCloseEvent event);
