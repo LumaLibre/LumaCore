0b1f1f3af1ab00452f52d9a2d8dfdc72f27915f1
Jsinco <jonah@jsinco.dev>
2026-05-25 16:36:16 -0400

    [ci skip] Remove debug stuff

--- src/main/java/dev/lumas/core/LumaCore.java
+++ src/main/java/dev/lumas/core/LumaCore.java
@@ -1,7 +1,6 @@
 package dev.lumas.core;
 
 import dev.lumas.core.listener.GuiListeners;
-import dev.lumas.core.manager.Modules;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -14,18 +13,14 @@ public class LumaCore extends JavaPlugin {
     @Getter
     private static boolean stopping;
 
-    private static Modules modules;
-
     @Override
     public void onLoad() {
         instance = this;
-        modules = new Modules(this);
         withPlaceholderAPI = getServer().getPluginManager().getPlugin("PlaceholderAPI") != null;
     }
 
     @Override
     public void onEnable() {
-        modules.register();
         // Not worth using a module manager for this
         getServer().getPluginManager().registerEvents(new GuiListeners(), this);
     }
@@ -34,6 +29,5 @@ public class LumaCore extends JavaPlugin {
     @Override
     public void onDisable() {
         stopping = true;
-        modules.unregister();
     }
 }
